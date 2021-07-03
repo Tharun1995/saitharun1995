4fea2a18f29b92ba3acd0e507a2040e33af779da
Tharun1995 <nomulatharunkumar@mail.com>
2021-07-04 00:07:57 +0530

    new class aded demo9 in github

---
+++ src/main/java/gitsample/gitpractice/Demo9.java
@@ -0,0 +1,15 @@
+package gitsample.gitpractice;
+
+import org.testng.annotations.Test;
+
+public class Demo9 {
+	@Test
+	private void solve() {
+    
+		System.out.println("hai tharun kumar");
+
+	}
+	
+	
+
+}
