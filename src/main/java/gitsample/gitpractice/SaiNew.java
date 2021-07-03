69cdf341b315d5396bd12a7a4e6bd77db41927ef
Tharun1995 <nomulatharunkumar@mail.com>
2021-07-04 00:24:21 +0530

    another file upload

---
+++ src/main/java/gitsample/gitpractice/SaiNew.java
@@ -0,0 +1,13 @@
+package gitsample.gitpractice;
+
+import org.testng.annotations.Test;
+
+public class SaiNew {
+	@Test
+	private void solve() {
+    
+		System.out.println("hai new file");
+
+	}
+	
+}
