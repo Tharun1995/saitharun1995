7c18816ca23ca9ce13a19f2d3f1edf26d5e2fafc
Tharun1995 <nomulatharunkumar@mail.com>
2021-07-03 23:53:16 +0530

    my first project in github

---
+++ src/main/java/gitsample/gitpractice/App.java
@@ -0,0 +1,14 @@
+package gitsample.gitpractice;
+
+import org.testng.annotations.Test;
+
+public class App 
+{
+   @Test
+    private void sai() {
+		// TODO Auto-generated method stub
+	   System.out.println( "Hello World!" );
+	    
+	}
+        
+}
